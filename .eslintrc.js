9ce07aa673ac78daa26df96bc5b54fbbf2261c30
Erik Neville <erikeneville@gmail.com>
2020-10-03 18:56:42 -0700

    akjidghadkg

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
     },
   ],
   rules: {
+    "no-use-before-define": "off",
     "@typescript-eslint/camelcase": "off",
     "@typescript-eslint/explicit-module-boundary-types": "off",
     "@typescript-eslint/no-explicit-any": "off",
